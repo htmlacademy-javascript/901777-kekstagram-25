e39f6983a29a097069492f2e8c1fe58cafce8585
Ivan <ticha385@gmail.com>
2022-02-24 12:55:21 +0300

    Дорабатываем первые функции

--- js/main.js
+++ js/main.js
@@ -1,12 +1,9 @@
 //Функия выводит рандомное число из переданного диапозона
 const getRandomNumber = function (min,max) {
-  if (min>=0 && max>=0){
+  if ((min>=0 && max>=0) && (min<max)){
     min = Math.ceil(min); //делаем целое число, округляем в меньшую сторону
     max = Math.floor(max); // делаем целове число округляя в большую сторону
     return Math.floor(Math.random() * (max - min + 1)) + min; //возвращаем рандомное число
-  }else{
-    // eslint-disable-next-line no-console
-    return console.log('Фнукция getRandomNumber принимает в себя только положительные числа');
   }
 };
 
