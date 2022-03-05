eefbec99b9a765c73d9778419d1aa3504594e3ab
Keks <keks@htmlacademy.ru>
2022-03-05 18:29:11 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/main.js
@@ -0,0 +1,8 @@
+/* eslint-disable prefer-const */
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+// import {getRandomNumber,getMaxLength,getRandomElem,getCounter} from './util.js';
+import {createPhotosArr} from './data-photos.js';
+
+console.log(createPhotosArr);
+
