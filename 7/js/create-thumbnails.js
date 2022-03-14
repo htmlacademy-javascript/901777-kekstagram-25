eb6879aa33e014daa8122305b24fcea609c70884
Keks <keks@htmlacademy.ru>
2022-03-14 22:07:59 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/create-thumbnails.js
@@ -0,0 +1,17 @@
+import {createPhotosArr} from './data-photos.js';
+
+const pictureTemplate = document.querySelector('#picture').content;
+const picturesContainer = document.querySelector('.pictures');
+const picturesFragment = document.createDocumentFragment(); //создание фрагмента
+
+
+//генерируем пикчи через перебор массива createPhotosArr
+createPhotosArr.forEach((elem) =>{
+  const pictureElement = pictureTemplate.cloneNode(true);
+  pictureElement.querySelector('img').src = elem.url;
+  pictureElement.querySelector('.picture__likes').textContent = elem.likes;
+  pictureElement.querySelector('.picture__comments').textContent = elem.comments.length;
+  picturesFragment.appendChild(pictureElement);
+});
+
+picturesContainer.appendChild(picturesFragment);
---
+++ 7/js/main.js
@@ -0,0 +1,3 @@
+import './create-thumbnails.js';
+
+
