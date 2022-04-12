a78337bf3af5d88d7b86263d8cb705ab3845cb76
Keks <keks@htmlacademy.ru>
2022-04-12 21:26:52 +0300

    :heavy_check_mark: Сборка #17

---
+++ 17/js/server.js
@@ -0,0 +1,21 @@
+import {createThumbnails} from './gallery.js';
+import { imgFilter } from './filters.js';
+
+const PICTURES_DATA = 'https://25.javascript.pages.academy/kekstagram/data';
+const FORM_SUBMIT_URL = 'https://25.javascript.pages.academy/kekstagram';
+
+fetch(PICTURES_DATA)
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error(`${response.status} ${response.statusText}`);
+  }).then((data) => {
+    createThumbnails(data);
+    imgFilter.classList.remove('img-filters--inactive');
+  }).catch((err) => {
+    // eslint-disable-next-line no-alert
+    alert(err);
+  });
+
+export {FORM_SUBMIT_URL,PICTURES_DATA};
---
+++ 17/js/util.js
@@ -0,0 +1,52 @@
+/* eslint-disable no-use-before-define */
+export {getRandomNumber, getMaxLength,getRandomElem, getCounter,debounce};
+
+//Функия выводит рандомное число из переданного диапозона
+const getRandomNumber = function (min,max) {
+  if ((min>=0 && max>=0) && (min<max)){
+    min = Math.ceil(min); //делаем целое число, округляем в меньшую сторону
+    max = Math.floor(max); // делаем целове число округляя в большую сторону
+    return Math.floor(Math.random() * (max - min + 1)) + min; //возвращаем рандомное число
+  }
+};
+
+
+//Функция проверки длины строки
+const getMaxLength = function(string, maxLength){
+  if(string.length <= maxLength){
+    return true;
+  }else{
+    return false;
+  }
+};
+
+//Функция получает рандомный элемент из массива
+const getRandomElem = function (arr){
+  return arr[getRandomNumber(0,arr.length-1)];
+};
+
+//Вводим функцию счетчики для того чтобы значения не повторялись в нужных значениях объекта
+function getCounter() {
+  let counter = 1;
+  return function() {
+    return counter++;
+  };
+}
+
+function debounce (callback, timeoutDelay = 500) {
+  // Используем замыкания, чтобы id таймаута у нас навсегда приклеился
+  // к возвращаемой функции с setTimeout, тогда мы его сможем перезаписывать
+  let timeoutId;
+
+  return (...rest) => {
+    // Перед каждым новым вызовом удаляем предыдущий таймаут,
+    // чтобы они не накапливались
+    clearTimeout(timeoutId);
+
+    // Затем устанавливаем новый таймаут с вызовом колбэка на ту же задержку
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+
+    // Таким образом цикл «поставить таймаут - удалить таймаут» будет выполняться,
+    // пока действие совершается чаще, чем переданная задержка timeoutDelay
+  };
+}
